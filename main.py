52b2c459fb3b789d251ea2738e5e0fa9d89261a6
SaiWinMaung <saiwinmaung.mc@gmail.com>
2024-12-30 18:55:51 +0630

    config page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-
+st.set_page_config(layout= 'wide', page_title='Git Exercise', page_icon= ':dart:')
 st.title('Git and GitHub Exercise')
 st.write('Author Name is Sai Win Maung')
 
